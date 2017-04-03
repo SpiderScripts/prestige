d3af922c59d6629ea74023675c5916ae39862948
SpiderScripts <peter@peteandemma.co.uk>
2017-04-03 18:57:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -53,13 +53,13 @@ function resetPrestige() {
 		coins: 0,
 		prestiges: [0,0,0,0,0,0,0,0,0,0]
 	};
-	localStorage.setItem("PrestigeSave",JSON.stringify(data);
+	localStorage.PrestigeSave = JSON.stringify(data);
 	cycle = setInterval(function () { tick(); }, 100);
 }
 
 function update() {
 	data.coins += getGain();
-	localStorage.setItem("PrestigeSave",JSON.stringify(data);
+	localStorage.PrestigeSave = JSON.stringify(data);
 }
 
 function draw() {
@@ -78,8 +78,8 @@ function draw() {
 }
 
 window.addEventListener("load",function () {
-	if (localStorage.getItem("PrestigeSave")) {
-		data = JSON.parse(localStorage.getItem("PrestigeSave"));
+	if (localStorage.PrestigeSave) {
+		data = JSON.parse(localStorage.PrestigeSave);
 	}
 	draw();
 	for (var i = 0; i < 10; i++) {
