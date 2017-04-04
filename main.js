ad82cf0c214664558c713258bb9b7c32e090aaf3
SpiderScripts <peter@peteandemma.co.uk>
2017-04-04 01:40:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function getRequirement(id) {
 	}
 }
 
-function canActivatePrestige(id) {
+function canActivatePrestige(id,no) {
 	if (id===0) {
 		return (data.coins >= getRequirement(0));
 	} else {
@@ -34,7 +34,7 @@ function canActivatePrestige(id) {
 	}
 }
 
-function activatePrestige(id) {
+function activatePrestige(id,no) {
 	if (canActivatePrestige(id)) {
 			if (id===0) {
 				data.coins -= getRequirement(0);
@@ -70,10 +70,15 @@ function draw() {
 		document.getElementById("tier"+(i+1)+"cost").innerHTML = getRequirement(i);
 		document.getElementById("tier"+(i+1)+"a").innerHTML = el;
 		document.getElementById("tier"+(i+1)+"mul").innerHTML = "x"+(el+1);
-		if (canActivatePrestige(i)) {
+		if (canActivatePrestige(i,1)) {
 			document.getElementById("tier"+(i+1)+"btn").disabled = false;
 		} else {
 			document.getElementById("tier"+(i+1)+"btn").disabled = true;
+		}
+		if (canActivatePrestige(i,10)) {
+			document.getElementById("tier"+(i+1)+"btn10").disabled = false;
+		} else {
+			document.getElementById("tier"+(i+1)+"btn10").disabled = true;
 		}
 	})
 }
@@ -88,7 +93,15 @@ window.addEventListener("load",function () {
 			"click",
 			(function(n) {
 				return (function () {
-					activatePrestige(n);
+					activatePrestige(n,1);
+				})
+			}(i))
+		);
+		document.getElementById("tier"+(i+1)+"btn10").addEventListener(
+			"click",
+			(function(n) {
+				return (function () {
+					activatePrestige(n,10);
 				})
 			}(i))
 		);
